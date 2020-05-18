c6d65fe0299ed23686a1a01f46c6715f20cf5c46
Bradley Matusiak <bmatusiak@gmail.com>
2020-05-18 15:09:23 -0400

    Update index.js

--- OnlyKey-SEA/index.js
+++ OnlyKey-SEA/index.js
@@ -11,7 +11,7 @@ define(function(require, exports, module) {
             pageLayout.find("#connect_onlykey").hide();
             pageLayout.find("#connected_onlykey").show();
             
-            pageLayout.find("#derive_public_key").click();
+            //pageLayout.find("#derive_public_key").click();
         },function(status){
             pageLayout.find("#connection_status").text(status)
         });
@@ -21,19 +21,22 @@ define(function(require, exports, module) {
     pageLayout.find("#derive_public_key").click(function(){
         onlykey.derive_public_key(async function(err,key,keyString){
             
-            pageLayout.find("#onlykey_pubkey").val(key.join(""))
+            pageLayout.find("#onlykey_pubkey").val(key)
             
-            $("#encryptKey").val("33333333333333333333333333333333")
-            $("#decryptKey").val("33333333333333333333333333333333")
+            if($("#encryptKey").val() =="")
+                $("#encryptKey").val(key)
+            
+            if($("#decryptKey").val() =="")
+                $("#decryptKey").val(key)
             
             
             pageLayout.find("#encryptData").val("test");
-            $("#encryptBTN").click();
+            //$("#encryptBTN").click();
         });
     });
     
     
-    pageLayout.find("#connect_onlykey").click();
+    //pageLayout.find("#connect_onlykey").click();
     
     
     $("#main-container").html(pageLayout);
@@ -41,7 +44,7 @@ define(function(require, exports, module) {
     $("#encryptBTN").click(async function(){
         
         var encData = pageLayout.find("#encryptData").val()
-        var encryptoToKey = pageLayout.find("#encryptKey").val().split("")
+        var encryptoToKey = pageLayout.find("#encryptKey").val();//.split("")
              //onlykey.b642bytes()
         
         onlykey.derive_shared_secret(encryptoToKey, async function(err,sharedSecret){ 
@@ -50,7 +53,7 @@ define(function(require, exports, module) {
             
             //pageLayout.find("#encryptData").val(enc);
             pageLayout.find("#decryptData").val(enc);
-            pageLayout.find("#pills-decrypt-tab").click();
+            //pageLayout.find("#pills-decrypt-tab").click();
         });
 
         
@@ -59,7 +62,7 @@ define(function(require, exports, module) {
     $("#decryptBTN").click(async function(){
         
         var decData = pageLayout.find("#decryptData").val()
-        var decryptoToKey = onlykey.b642bytes(pageLayout.find("#decryptKey").val())
+        var decryptoToKey = pageLayout.find("#decryptKey").val()
         
         onlykey.derive_shared_secret(decryptoToKey, async function(err,sharedSecret){ 
             
@@ -68,7 +71,7 @@ define(function(require, exports, module) {
             var dec = await GUN.SEA.decrypt(decData, sharedSecret);
             
             pageLayout.find("#encryptData").val(dec);
-            pageLayout.find("#pills-encrypt-tab").click();
+            //pageLayout.find("#pills-encrypt-tab").click();
         });
 
         
